87a21bac77ed9ce8481e68a2c2c72b0f8036e8c4
{kaplunovsky} <{kaplunovski@mail.ru}>
2022-02-18 19:19:22 +0300

    init commit

--- src/main/java/RunTest.java
+++ src/main/java/RunTest.java
@@ -10,7 +10,7 @@ public class RunTest {
 
     public void main(){
         open("https://demoqa.com/automation-practice-form");
-        // тест коммент 2
+        // тест коммент
     }
 
     @Before
